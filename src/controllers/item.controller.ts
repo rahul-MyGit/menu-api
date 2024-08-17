eb99f2faa38868a0179791b499d5cc727dfbb4e1
Rahul <rahul.mymail1@gmail.com>
2024-08-17 22:27:01 +0530

    Add: all items route

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -15,3 +15,108 @@ export const createItemController = async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Error while creating the items'});
     }
 };
+
+export const getAllItemsController = async (req: Request, res: Response) => {
+    try {
+      const items = await prisma.item.findMany();
+      res.json(items);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching items' });
+    }
+}
+
+
+export const getItemsUnderCategoryController = async (req: Request, res: Response) => {
+    try {
+      const { categoryId } = req.params;
+      const items = await prisma.item.findMany({
+        where: { categoryId: parseInt(categoryId) },
+      });
+      res.json(items);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching items' });
+    }
+}
+
+export const getItemsUnderSubCategoryController = async (req: Request, res: Response) => {
+    try {
+      const { subCategoryId } = req.params;
+      const items = await prisma.item.findMany({
+        where: { subCategoryId: parseInt(subCategoryId) },
+      });
+      res.json(items);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching items' });
+    }
+}
+
+// export const getItemByNameController = async (req: Request, res: Response) => {
+//     try {
+//       const { name } = req.params;
+//       const items = await prisma.item.findMany({
+//         where: {
+//           name: {
+//             contains: name,
+//             mode: 'insensitive',
+//           },
+//         },
+//       });
+//       res.json(items);
+//     } catch (error) {
+//       res.status(500).json({ error: 'Error searching items' });
+//     }
+// };
+
+
+export const getItemByNameOrIdController =  async (req: Request, res: Response) => {
+    try {
+      const { identifier } = req.params;
+      const item = await prisma.item.findFirst({
+        where: {
+          OR: [
+            { id: parseInt(identifier) || undefined },
+            { name: identifier },
+          ],
+        },
+      });
+      if (item) {
+        res.json(item);
+      } else {
+        res.status(404).json({ error: 'Item not found' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching item' });
+    }
+};
+
+export const editItemController = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const { name, image, description, taxApplicability, tax, baseAmount, discount } = req.body;
+      const totalAmount = baseAmount - discount;
+      const updatedItem = await prisma.item.update({
+        where: { id: parseInt(id) },
+        data: { name, image, description, taxApplicability, tax, baseAmount, discount, totalAmount },
+      });
+      res.json(updatedItem);
+    } catch (error) {
+      res.status(500).json({ error: 'Error updating item' });
+    }
+}
+
+export const getItemsByNameController = async (req: Request, res: Response) => {
+    try {
+      const { name } = req.params;
+      const items = await prisma.item.findMany({
+        where: {
+          name: {
+            contains: name,
+            mode: 'insensitive',
+          },
+        },
+      });
+      res.json(items);
+    } catch (error) {
+      res.status(500).json({ error: 'Error searching items' });
+    }
+}
--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createItemController } from "../controllers/item.controller";
+import { createItemController,getAllItemsController, getItemsUnderCategoryController, getItemsUnderSubCategoryController, getItemByNameOrIdController, editItemController, getItemsByNameController } from "../controllers/item.controller";
 import { itemSchema} from "../utils/zod-schemas";
 import { validateSchema } from "../middlewares/validation.middleware";
 
 const router = express.Router();
 
 router.post('/', validateSchema(itemSchema) ,createItemController);
+router.get('/', getAllItemsController);
+router.get('/:categoryId', getItemsUnderCategoryController);
+router.get('/:subCategoryId', getItemsUnderSubCategoryController);
+router.get('/:identifier', getItemByNameOrIdController);
+router.put('/:id', validateSchema(itemSchema) ,editItemController );
+router.get('/search/:name', getItemsByNameController);
 
 export default router;
